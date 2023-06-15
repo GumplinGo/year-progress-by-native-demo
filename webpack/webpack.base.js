869041a48f9cfdc923826308a2849f69b205c360
PeterGuo <guojianglin123@gmail.com>
2023-06-15 22:58:55 +0800

    fix github pages

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -18,7 +18,7 @@ module.exports = {
   entry: path.join(__dirname, '../src/index.js'),
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, '../docs'),
   },
   module: {
     rules: [
